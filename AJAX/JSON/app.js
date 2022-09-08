c57c709b990aacefa45d7e82a6dc75369dfbec8d
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-08 10:06:58 -0500

    JSON

---
+++ AJAX/JSON/app.js
@@ -0,0 +1,4 @@
+// THIS IS A STRING OF JSON from https://api.cryptonator.com/api/ticker/btc-usd
+const data = `{"ticker":{"base":"BTC","target":"USD","price":"19338.65659772","volume":"15574.71487097","change":"141.81936408"},"timestamp":1662649323,"success":true,"error":""}`
+
+
