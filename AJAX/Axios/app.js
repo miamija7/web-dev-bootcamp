efffc545fd72b7465c20080dbfb6de633dd72520
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-08 13:51:54 -0500

    Axios Requests with Headers

--- AJAX/Axios/app.js
+++ AJAX/Axios/app.js
@@ -15,15 +15,56 @@
 // loadStarWarsPerson();
 
 
-// REFACTOR TO ACCEPT AN :ID
-const loadStarWarsPerson = async (id) => {
+// // REFACTOR TO ACCEPT AN :ID
+// const loadStarWarsPerson = async (id) => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+//         console.log(res.data);
+//     } catch (e) {
+//         console.log("ERROR: ", e);
+//     }
+// }
+// loadStarWarsPerson(7);
+
+
+// // DAD JOKES: HEADER IN HTTP REQUEST
+// const getDadJoke = async () => {
+//     try {
+//         const config = { headers : { Accept: "application/json"} }
+//         const res = await axios.get('https://icanhazdadjoke.com/', config);
+//         console.log(res.data.joke);
+//     } catch (e) {
+//         console.log("ERROR:", e);
+//     }
+// }
+// // getDadJoke();
+
+
+// DAD JOKES ON PAGE GENERATOR
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
     try {
-        const res = await axios.get(`https://swapi.dev/api/people/${id}`);
-        console.log(res.data);
+        const jokeText = await getDadJoke()
+        const newLI = document.createElement('li');
+        newLI.append(jokeText);
+        jokes.append(newLI);
     } catch (e) {
-        console.log("ERROR: ", e);
+        console.log("ERROR:", e);
+    }
+
+}
+const getDadJoke = async () => {
+    try {
+        const config = { headers : { Accept: "application/json"} }
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
     }
 }
-loadStarWarsPerson(1);
-loadStarWarsPerson(5);
-loadStarWarsPerson(7);
+
+button.addEventListener('click', addNewJoke);
+
+
