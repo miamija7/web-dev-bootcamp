59c90cf0b4d0c9492e94f944ca5291b8578a4fb4
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-04 10:56:24 -0500

    factory functions

--- Prototypes, Classes, & OOP/prototypes/app.js
+++ Prototypes, Classes, & OOP/prototypes/app.js
@@ -1,5 +1,12 @@
 // Create a prototype
+String.prototype.yell = function () {
+    console.log(this.toUpperCase());
+};
 
+// Override a prototype
+Array.prototype.pop = function () {
+    return 'SORRY I WANT THAT ELEMENT, I WILL NEVER POP!'
+}
 
 
 
